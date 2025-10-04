09485dfc57ab7dff5e001109139a18944310c110
VGCoding <vgcoding05@gmail.com>
2025-10-04 13:48:09 +0530

    Frontend change, added env for handling backend

--- src/util/api.js
+++ src/util/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Create the Axios instance with a base configuration
 const api = axios.create({
-  baseURL: import.meta.env.API_BASE_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
   withCredentials: true,
 });
 
