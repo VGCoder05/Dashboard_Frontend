e59fc8b2ca068967b377554f19c39bae7da552bb
VGCoding <vgcoding05@gmail.com>
2025-10-04 13:58:48 +0530

    Changed way of routing to 'HashRouter'

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ContextProvider from "./context/ContextProvider.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
